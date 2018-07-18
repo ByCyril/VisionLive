7a79f3878e0400034e6e50ed644f65ac02d998a4
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-07-17 21:50:46 -0700

    Initial Commit

---
+++ VisionLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionLive
+//
+//  Created by Cyril Garcia on 7/17/18.
+//  Copyright © 2018 Cyril Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
